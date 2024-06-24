873e7d9d3fb66391da7fc473e03c310665ed9667
SaidLamghari <said.lmg@gmail.com>
2024-06-24 19:59:55 +0000

    1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+// Modifier par : SAID LAMGHARI
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
+  return [task, task2];
+}
