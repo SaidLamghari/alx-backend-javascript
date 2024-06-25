8bba8e2b0964aab8ed56bc5cbaa96e690374641e
SaidLamghari <said.lmg@gmail.com>
2024-06-25 22:31:47 +0000

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+// Autor : SAID LAMGHARI
+// 100-await.js
+
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+}
