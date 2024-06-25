4f9eb41dcb2932e51fda4d8279cda70b25a1f726
SaidLamghari <said.lmg@gmail.com>
2024-06-25 22:20:54 +0000

    9-try.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+// Autor : SAID LAMGHARI
+// 9-try.js
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
