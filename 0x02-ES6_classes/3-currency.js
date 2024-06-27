1c53dea9eb5a1cfb43c64cbb149a865a02521509
SaidLamghari <said.lmg@gmail.com>
2024-06-27 16:42:49 +0000

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+// Autor SAID LAMGHARI
+// Définition de la classe Currency
+export default class Currency {
+  // Constructeur avec les attributs : code (String), name (String)
+  constructor(code, name) {
+    // Stockage des attributs avec un underscore préfixé
+    this._code = code; // Code de la devise (ex: '$')
+    this._name = name; // Nom de la devise (ex: 'Dollars')
+  }
+
+  // Getter pour l'attribut code
+  get code() {
+    return this._code;
+  }
+
+  // Setter pour l'attribut code
+  set code(nwCode) {
+    this._code = nwCode;
+  }
+
+  // Getter pour l'attribut name
+  get name() {
+    return this._name;
+  }
+
+  // Setter pour l'attribut name
+  set name(nwName) {
+    this._name = nwName;
+  }
+
+  // Méthode pour afficher les attributs dans un format spécifié
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`; // Format: nom (code)
+  }
+}
