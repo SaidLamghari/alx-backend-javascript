2ac0f9c9c7ff5bcfe55c00a2b4163d67764cd8cf
SaidLamghari <said.lmg@gmail.com>
2024-06-27 15:34:16 +0000

    0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+// Autor SAID LAMGHARI
+// Définition de la classe ClassRoom avec un export par défaut
+export default class ClassRoom {
+  // Constructeur de la classe, prend maxStudentsSize comme paramètre
+  constructor(maxStudentsSize) {
+    // Assignation de maxStudentsSize à la propriété privée _maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
