0dff01a993e243938c269d67f7ad89830f6c63e8
SaidLamghari <said.lmg@gmail.com>
2024-06-28 00:15:37 +0000

    10-car.js 100-evcar

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,27 +1,51 @@
 // Autor SAID LAMGHARI
-// Définir des symboles pour les champs privés
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-// Exporter la classe par défaut
 export default class Car {
-  // Constructeur de la classe Car
   constructor(brand, motor, color) {
-    // Initialisation des champs privés avec les valeurs passées en paramètres
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    // Initialisation des propriétés publiques à l'aide des paramètres du constructeur
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
-  // Méthode pour cloner une instance de voiture
-  cloneCar() {
-    // Accéder aux champs privés en utilisant les symboles
-    const brand = this[_brand];
-    const motor = this[_motor];
-    const color = this[_color];
+  // Getter et Setter pour la propriété 'brand' à l'aide des champs privés
+  get brand() {
+    return this._brand; // Retourne la valeur du champ privé _brand
+  }
 
-    // Créer une nouvelle instance du même type de voiture avec les mêmes valeurs
-    return new this.constructor(brand, motor, color);
+  set brand(vl) {
+    this._brand = vl; // Définit la valeur du champ privé _brand avec la valeur passée
+  }
+
+  // Getter et Setter pour la propriété 'motor' à l'aide des champs privés
+  get motor() {
+    return this._motor; // Retourne la valeur du champ privé _motor
+  }
+
+  set motor(vl) {
+    this._motor = vl; // Définit la valeur du champ privé _motor avec la valeur passée
+  }
+
+  // Getter et Setter pour la propriété 'color' à l'aide des champs privés
+  get color() {
+    return this._color; // Retourne la valeur du champ privé _color
+  }
+
+  set color(vl) {
+    this._color = vl; // Définit la valeur du champ privé _color avec la valeur passée
+  }
+
+  // Définition d'une propriété statique Symbol.species pour la classe Car
+  static get [Symbol.species]() {
+    return this; // Retourne la classe elle-même (Car) pour indiquer le type par défaut
+  }
+
+  // Méthode pour cloner une instance de Car
+  cloneCar() {
+    // Récupère le constructeur de la classe actuelle à partir de Symbol.species
+    const Cars = this.constructor[Symbol.species];
+
+    // Crée et retourne une nouvelle instance de
+    // la classe spécifiée par Symbol.species (par défaut, la classe Car)
+    return new Cars();
   }
 }
