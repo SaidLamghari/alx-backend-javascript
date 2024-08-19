66ff0cce7f7855eb3f374e0254e0fe9da31be905
SaidLamghari <said.lmg@gmail.com>
2024-08-19 19:20:53 +0000

    eslint

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,15 +4,15 @@
  * Cette fonction utilise `console.log` pour afficher le message à l'écran.
  *
  * @param {string} message - Le message à afficher dans la console.
- * 
+ *
  * Auteur SAID LAMGHARI
  */
 function displayMessage(message) {
-    console.log(message);
-  }
-  
-  // Exporte la fonction `displayMessage` pour
-  // qu'elle puisse être utilisée dans d'autres fichiers.
-  // Cette ligne permet à `0-main.js` d'importer
-  // et d'utiliser la fonction `displayMessage`.
-  module.exports = displayMessage;
+  console.log(message);
+}
+
+// Exporte la fonction `displayMessage` pour
+// qu'elle puisse être utilisée dans d'autres fichiers.
+// Cette ligne permet à `0-main.js` d'importer
+// et d'utiliser la fonction `displayMessage`.
+module.exports = displayMessage;
