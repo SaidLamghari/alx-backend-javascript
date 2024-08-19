07b6859b16ebddfd11dd3e13e34e73f1ca3224d6
SaidLamghari <said.lmg@gmail.com>
2024-08-19 18:17:50 +0000

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,18 @@
+/**
+ * Affiche le message fourni dans la sortie standard (STDOUT).
+ *
+ * Cette fonction utilise `console.log` pour afficher le message à l'écran.
+ *
+ * @param {string} message - Le message à afficher dans la console.
+ * 
+ * Auteur SAID LAMGHARI
+ */
+function displayMessage(message) {
+    console.log(message);
+  }
+  
+  // Exporte la fonction `displayMessage` pour
+  // qu'elle puisse être utilisée dans d'autres fichiers.
+  // Cette ligne permet à `0-main.js` d'importer
+  // et d'utiliser la fonction `displayMessage`.
+  module.exports = displayMessage;
