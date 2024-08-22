f248ec33b378b11e464dc673713db0ace1dbe1d6
SaidLamghari <said.lmg@gmail.com>
2024-08-22 17:47:49 +0000

    api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,29 @@
 // 8-api/api.js
-
+// Auteur SAID LAMGHARI
+// Importation du module 'express', qui est un
+// framework pour créer des serveurs web en Node.js
 const express = require('express');
+
+// Création d'une application Express
 const app = express();
+
+// Définition du port sur lequel
+// le serveur écoutera les requêtes
 const PORT = 7865;
 
-app.get('/', (req, res) => {
+// Définition d'une route GET pour le chemin racine ('/')
+app.get('/', (_, res) => {
+  // Envoi d'une réponse simple au client
   res.send('Welcome to the payment system');
 });
 
+// Démarrage du serveur sur le port défini
 app.listen(PORT, () => {
+  // Message affiché dans la console lorsque
+  // le serveur démarre avec succès
   console.log(`API available on localhost port ${PORT}`);
 });
 
+// Exportation de l'application Express pour qu'elle puisse
+// être utilisée dans d'autres modules ou fichiers
+module.exports = app;
