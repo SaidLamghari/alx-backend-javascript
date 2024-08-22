bd7fdcb259caf5c8fd2586fb2d952f3582ab3dd0
SaidLamghari <said.lmg@gmail.com>
2024-08-22 20:31:26 +0000

    10-api/api.test.js

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,8 +1,11 @@
 // Auteur SAID LAMGHARI
+// Importation de la fonction 'expect' de
+// la bibliothèque 'chai' pour les assertions
+const { expect } = require('chai');
+
 // Importation du module 'request' pour effectuer des requêtes HTTP
 const request = require('request');
-// Importation de la fonction 'expect' de la bibliothèque 'chai' pour les assertions
-const { expect } = require('chai');
+
 
 // Définition d'un groupe de tests pour les intégrations API
 describe('API integration test', () => {
@@ -12,11 +15,11 @@ describe('API integration test', () => {
   // Test pour vérifier la réponse correcte de la route GET /
   it('GET / returns correct response', (done) => {
     // Effectuer une requête GET à la route racine de l'API
-    request.get(`${API_LNK}/`, (_err, rs, bdy) => {
+    request.get(`${API_LNK}/`, (_err, res, body) => {
       // Vérifier que le code de statut HTTP est 200 (OK)
-      expect(rs.statusCode).to.be.equal(200);
+      expect(res.statusCode).to.be.equal(200);
       // Vérifier que le corps de la réponse est le texte attendu
-      expect(bdy).to.be.equal('Welcome to the payment system');
+      expect(body).to.be.equal('Welcome to the payment system');
       // Indiquer que le test est terminé avec succès
       done();
     });
@@ -24,12 +27,13 @@ describe('API integration test', () => {
 
   // Test pour vérifier la réponse correcte pour une route GET avec un ID valide
   it('GET /cart/:id returns correct response for valid :id', (done) => {
-    // Effectuer une requête GET à la route '/cart/417' avec un ID valide
-    request.get(`${API_LNK}/cart/417`, (_err, rs, bdy) => {
+    // Effectuer une requête GET à la route '/cart/47' avec un ID valide
+    request.get(`${API_LNK}/cart/47`, (_err, res, body) => {
       // Vérifier que le code de statut HTTP est 200 (OK)
-      expect(rs.statusCode).to.be.equal(200);
-      // Vérifier que le corps de la réponse correspond au texte attendu avec l'ID fourni
-      expect(bdy).to.be.equal('Payment methods for cart 417');
+      expect(res.statusCode).to.be.equal(200);
+      // Vérifier que le corps de la réponse
+      // correspond au texte attendu avec l'ID fourni
+      expect(body).to.be.equal('Payment methods for cart 47');
       // Indiquer que le test est terminé avec succès
       done();
     });
@@ -38,26 +42,25 @@ describe('API integration test', () => {
   // Test pour vérifier que la route GET renvoie une
   // erreur 404 pour des valeurs négatives dans :id
   it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
-    // Effectuer une requête GET à la route '/cart/-417' avec un ID négatif
-    request.get(`${API_LNK}/cart/-417`, (_err, rs, _bdy) => {
+    // Effectuer une requête GET à la route '/cart/-47' avec un ID négatif
+    request.get(`${API_LNK}/cart/-47`, (_err, res, _body) => {
       // Vérifier que le code de statut HTTP est 404 (Not Found)
-      expect(rs.statusCode).to.be.equal(404);
+      expect(res.statusCode).to.be.equal(404);
       // Indiquer que le test est terminé avec succès
       done();
     });
   });
 
-  // Test pour vérifier que la route GET renvoie
-  // une erreur 404 pour des valeurs non numériques dans :id
+  // Test pour vérifier que la route GET renvoie une
+  // erreur 404 pour des valeurs non numériques dans :id
   it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
     // Effectuer une requête GET à la route
     // '/cart/d200-44a5-9de6' avec un ID non numérique
-    request.get(`${API_LNK}/cart/d200-44a5-9de6`, (_err, rs, _bdy) => {
+    request.get(`${API_LNK}/cart/d200-44a5-9de6`, (_err, res, _body) => {
       // Vérifier que le code de statut HTTP est 404 (Not Found)
-      expect(rs.statusCode).to.be.equal(404);
+      expect(res.statusCode).to.be.equal(404);
       // Indiquer que le test est terminé avec succès
       done();
     });
   });
 });
-
