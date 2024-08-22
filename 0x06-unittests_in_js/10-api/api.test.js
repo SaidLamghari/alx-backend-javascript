252484d27f37b59b6955cd2cb7dd9f75f00aa102
SaidLamghari <said.lmg@gmail.com>
2024-08-22 20:35:44 +0000

    10-api/api.test.js

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -15,11 +15,11 @@ describe('API integration test', () => {
   // Test pour vérifier la réponse correcte de la route GET /
   it('GET / returns correct response', (done) => {
     // Effectuer une requête GET à la route racine de l'API
-    request.get(`${API_LNK}/`, (_err, res, body) => {
+    request.get(`${API_LNK}/`, (_err, rs, bdy) => {
       // Vérifier que le code de statut HTTP est 200 (OK)
-      expect(res.statusCode).to.be.equal(200);
+      expect(rs.statusCode).to.be.equal(200);
       // Vérifier que le corps de la réponse est le texte attendu
-      expect(body).to.be.equal('Welcome to the payment system');
+      expect(bdy).to.be.equal('Welcome to the payment system');
       // Indiquer que le test est terminé avec succès
       done();
     });
@@ -27,13 +27,13 @@ describe('API integration test', () => {
 
   // Test pour vérifier la réponse correcte pour une route GET avec un ID valide
   it('GET /cart/:id returns correct response for valid :id', (done) => {
-    // Effectuer une requête GET à la route '/cart/47' avec un ID valide
-    request.get(`${API_LNK}/cart/47`, (_err, res, body) => {
+    // Effectuer une requête GET à la route '/cart/417' avec un ID valide
+    request.get(`${API_LNK}/cart/417`, (_err, rs, bdy) => {
       // Vérifier que le code de statut HTTP est 200 (OK)
-      expect(res.statusCode).to.be.equal(200);
+      expect(rs.statusCode).to.be.equal(200);
       // Vérifier que le corps de la réponse
       // correspond au texte attendu avec l'ID fourni
-      expect(body).to.be.equal('Payment methods for cart 47');
+      expect(bdy).to.be.equal('Payment methods for cart 417');
       // Indiquer que le test est terminé avec succès
       done();
     });
@@ -42,25 +42,26 @@ describe('API integration test', () => {
   // Test pour vérifier que la route GET renvoie une
   // erreur 404 pour des valeurs négatives dans :id
   it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
-    // Effectuer une requête GET à la route '/cart/-47' avec un ID négatif
-    request.get(`${API_LNK}/cart/-47`, (_err, res, _body) => {
+    // Effectuer une requête GET à la route '/cart/-417' avec un ID négatif
+    request.get(`${API_LNK}/cart/-417`, (_err, rs, _bdy) => {
       // Vérifier que le code de statut HTTP est 404 (Not Found)
-      expect(res.statusCode).to.be.equal(404);
+      expect(rs.statusCode).to.be.equal(404);
       // Indiquer que le test est terminé avec succès
       done();
     });
   });
 
-  // Test pour vérifier que la route GET renvoie une
-  // erreur 404 pour des valeurs non numériques dans :id
+  // Test pour vérifier que la route GET renvoie
+  // une erreur 404 pour des valeurs non numériques dans :id
   it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
     // Effectuer une requête GET à la route
     // '/cart/d200-44a5-9de6' avec un ID non numérique
-    request.get(`${API_LNK}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+    request.get(`${API_LNK}/cart/d200-44a5-9de6`, (_err, rs, _bdy) => {
       // Vérifier que le code de statut HTTP est 404 (Not Found)
-      expect(res.statusCode).to.be.equal(404);
+      expect(rs.statusCode).to.be.equal(404);
       // Indiquer que le test est terminé avec succès
       done();
     });
   });
 });
+
